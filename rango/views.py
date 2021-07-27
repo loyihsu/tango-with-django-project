0777034d063a25d1548710c2368df2cd1951910e
Yu-Sung Hsu <2540296h@student.gla.ac.uk>
2021-07-27 18:10:34 +0800

    Add templates and render index.html template for index request.

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<a href=\"/rango/about/\">About</a>Rango says hey there partner!")
+    # Construct a dictionary to pass to the template engine as its content.
+    # Key should match that of the template.
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    # Return a rendered response to the client using the method from shortcuts
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("<a href=\"/rango/\">Index</a>Rango says here is the about page.")
