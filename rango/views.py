107461be9625bb5c5e6e94a52b4dd7cff6908fc3
Yu-Sung Hsu <2540296h@student.gla.ac.uk>
2021-07-28 18:32:57 +0800

    Add category display to the index page.

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category
+
 def index(request):
-    # Construct a dictionary to pass to the template engine as its content.
-    # Key should match that of the template.
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
     # Return a rendered response to the client using the method from shortcuts
     return render(request, 'rango/index.html', context=context_dict)
 
